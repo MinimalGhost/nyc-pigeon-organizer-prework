11e490ffbf412993f09fa92cb4776728a6fcab9a
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-15 16:30:52 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -18,16 +18,22 @@ def nyc_pigeon_organizer(data)
           new_hash[name] = theo_stats
         when "Peter Jr."
           peter_stats.push(attribute.to_s)
+          new_hash[name] = peter_stats
         when "Lucky"
           lucky_stats.push(attribute.to_s)
+          new_hash[name] = lucky_stats
         when "Ms. K"
           miss_stats.push(attribute.to_s)
+          new_hash[name] = miss_stats
         when "Queenie"
           queenie_stats.push(attribute.to_s)
+          new_hash[name] = queenie_stats
         when "Andrew"
           andrew_stats.push(attribute.to_s)
+          new_hash[name] = andrew_stats
         when "Alex"
           alex_stats.push(attribute.to_s)
+          new_hash[name] = alex_stats
         end
       end
     end
