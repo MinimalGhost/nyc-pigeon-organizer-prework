4d366e7c830692af59b61ad909e1b8779e700573
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-15 16:22:28 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  theo_stats = []
+  data.each do |attribute, type|
+    type.each do |color, names|
+      names.each do |name|
+          binding.pry
+        if name == "Theo"
+          theo_stats.push(color.to_s)
+        end
+      end
+    end
+  end
+  theo_stats
 end
