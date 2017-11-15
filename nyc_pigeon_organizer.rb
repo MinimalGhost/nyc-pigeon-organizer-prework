fc61784a7c41e394e5099f061a4add94f4c4988b
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-15 16:32:52 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  new_hash = {}
+  pigeon_list = {}
   theo_stats = []
   peter_stats = []
   lucky_stats = []
@@ -15,29 +15,29 @@ def nyc_pigeon_organizer(data)
         case name
         when "Theo"
           theo_stats.push(attribute.to_s)
-          new_hash[name] = theo_stats
+          pigeon_list[name] = theo_stats
         when "Peter Jr."
           peter_stats.push(attribute.to_s)
-          new_hash[name] = peter_stats
+          pigeon_list[name] = peter_stats
         when "Lucky"
           lucky_stats.push(attribute.to_s)
-          new_hash[name] = lucky_stats
+          pigeon_list[name] = lucky_stats
         when "Ms. K"
           miss_stats.push(attribute.to_s)
-          new_hash[name] = miss_stats
+          pigeon_list[name] = miss_stats
         when "Queenie"
           queenie_stats.push(attribute.to_s)
-          new_hash[name] = queenie_stats
+          pigeon_list[name] = queenie_stats
         when "Andrew"
           andrew_stats.push(attribute.to_s)
-          new_hash[name] = andrew_stats
+          pigeon_list[name] = andrew_stats
         when "Alex"
           alex_stats.push(attribute.to_s)
-          new_hash[name] = alex_stats
+          pigeon_list[name] = alex_stats
         end
       end
     end
   end
-  new_hash
+  pigeon_list
   binding.pry
 end
