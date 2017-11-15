830fe26b7a4647cdd9f36ba7f11d726eac4e8a71
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-15 16:27:21 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,15 +3,43 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   new_hash = {}
   theo_stats = []
+  peter_stats = []
+  lucky_stats = []
+  miss_stats = []
+  queenie_stats = []
+  andrew_stats = []
+  alex_stats = []
   data.each do |attribute, type|
-    type.each do |color, names|
+    type.each do |attribute, names|
       names.each do |name|
-          binding.pry
-        if name == "Theo"
-          theo_stats.push(color.to_s)
+        case name
+        when "Theo"
+          theo_stats.push(attribute.to_s)
+        when "Peter Jr."
+          peter_stats.push(attribute.to_s)
+        when "Lucky"
+          lucky_stats.push(attribute.to_s)
+        when "Ms. K"
+          miss_stats.push(attribute.to_s)
+        when "Queenie"
+          queenie_stats.push(attribute.to_s)
+        when "Andrew"
+          andrew_stats.push(attribute.to_s)
+        when "Alex"
+          alex_stats.push(attribute.to_s)
         end
+        # if name == "Theo"
+        #   theo_stats.push(color.to_s)
+        # end
       end
     end
   end
   theo_stats
+  peter_stats
+  lucky_stats
+  miss_stats
+  queenie_stats
+  andrew_stats
+  alex_stats
+  binding.pry
 end
